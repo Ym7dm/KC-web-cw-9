98fe3d2bd67ea611ab8feab04fc210e72e9398fd
Yahya Mohamed <109866996+Ym7dm@users.noreply.github.com>
2022-08-25 12:24:39 +0300

    CW-9 Done

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let orders = [];
 
 function addOrder() {
   let name = document.getElementById("name").value;
   let id = document.getElementById("id").value;
   let phone = document.getElementById("phone").value;
   let order = document.getElementById("order").value;
-  
-  // أنشئ كائن newOrder
-  
-  // أدرج الكائن إلى المصفوفة
-  
-  // طبق الخطوة 7 تحت هذا الكومنت
-  
-}
 
+  // أنشئ كائن newOrder
+
+  let newOrder = {
+    order: order,
+    name: name,
+    id: id,
+    phone: phone,
+  };
+  // أدرج الكائن إلى المصفوفة
+  orders.push(newOrder);
+  // طبق الخطوة 7 تحت هذا الكومنت
+  load();
+}
 function load() {
   let container = document.getElementById("container");
   container.innerHTML = "";
   // طبّق الخطوة 6 تحت هذا الكومنت
 
+  orders.forEach((items) => {
+    container.innerHTML += `<div id="rem">
+    <h1>${items.name} - ${items.id}</h1><h3>${items.order}</h3>
+    </div>`;
+  });
+}
+function remove() {
+  let remDiv = document.getElementById("rem");
+  remDiv.remove();
 }
